2b5ccc6137171401ed3478b00c6580025a6692b6
George <gothentrek@gmail.com>
2021-11-10 22:51:54 +0000

    Changes made

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("We did it better than them");
+﻿using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+String variableName = "https://www.youtube.com/playlist?list=PLNF97D26mpE4VxDazVrsLShzp9xq5rSNC";
+WebRequest webRequest = HttpWebRequest.Create(variableName);
+WebResponse webResponse = webRequest.GetResponse();
+StreamReader streamReader = new StreamReader(webResponse.GetResponseStream());
+Console.WriteLine(streamReader.ReadToEnd());
+HtmlDocument htmlDocument = new HtmlDocument();
+htmlDocument.LoadHtml(streamReader.ReadToEnd());
+String someResult = htmlDocument.DocumentNode.SelectSingleNode("//div/contents").Attributes["class"].Value;
+Console.WriteLine(someResult);
+//HTMLparser htmlParse = new HTMLparser(streamReader.ToString());
